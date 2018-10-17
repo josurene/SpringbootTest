6d2d33977d7f0105b7e45023c1c413940f154974
josurene <josu@hotmail.com>
2018-10-17 15:04:42 -0700

    update

--- src/main/java/com/josu/work/demo/controllers/ProductController.java
+++ src/main/java/com/josu/work/demo/controllers/ProductController.java
@@ -4,6 +4,9 @@ package com.josu.work.demo.controllers;
 import com.josu.work.demo.domain.ProductDomain;
 import com.josu.work.demo.exception.ProductNotFoundException;
 import com.josu.work.demo.models.Product;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-//@Controller("/products")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -21,22 +23,40 @@ public class ProductController {
     ProductDomain productDomain;
 
     @GetMapping("/init")
+    @ApiOperation(value = "Init the database from the xml file")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully init"),
+            @ApiResponse(code = 500, message = "Error on the xml")
+    })
     public ResponseEntity init(){
         productDomain.init();
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping("/all")
+    @ApiOperation(value = "Gets all products")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get all products")
+    })
     public Iterable<Product> getProducts(){
         return productDomain.getAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get a product using it's id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully init"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
     public Product getProduct(@PathVariable Integer id) {
         return productDomain.get(id).orElseThrow(() -> new ProductNotFoundException("Product not found:"+id));
     }
 
     @PostMapping("")
+    @ApiOperation(value = "Upload a product")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully init")
+    })
     public ResponseEntity addProduct(@RequestBody Product product){
         productDomain.add(product);
         return new ResponseEntity(HttpStatus.OK);
--- src/main/java/com/josu/work/demo/exception/InitException.java
+++ src/main/java/com/josu/work/demo/exception/InitException.java
@@ -1,5 +1,9 @@
 package com.josu.work.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class InitException extends RuntimeException {
     public InitException() {
     }
